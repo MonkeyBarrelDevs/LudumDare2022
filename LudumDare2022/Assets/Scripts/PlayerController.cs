5a448e7ce101258d0da9bf6abed7258bcf219eeb
SABear-Bobby <89588537+SABear-Bobby@users.noreply.github.com>
2022-09-30 21:38:47 -0600

    Added Horizontal movement
    Using rigidbody2d.velocity to control character movement

--- LudumDare2022/Assets/Scripts/PlayerController.cs
+++ LudumDare2022/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //Editable Variables
+    [SerializeField] private int speed;
+
+    //Components
+    private Rigidbody2D playerBody;
+    private BoxCollider2D playerCollider;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    private void Awake()
+    {
+        playerBody = GetComponent<Rigidbody2D>();
+    }
+
     // Update is called once per frame
     void Update()
     {
+        /************\
+        |  Movement  |
+        \************/
+
+        //Left and Right Movement
+        playerBody.velocity = new Vector2(speed * Input.GetAxis("Horizontal"), playerBody.velocity.y);
+
+        
         
     }
+
 }
